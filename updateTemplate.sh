f7d708629c04437ee0d6421c9dc00364129dbb7b
Erwan Esnault <erwanesnault@hotmail.com>
2023-08-23 20:11:08 +0900

    V1.0.1

--- updateTemplate.sh
+++ updateTemplate.sh
@@ -2,24 +2,30 @@
 
 BACKUP_DIR="BACKUP_$(date +%Y%m%d_%H%M%S)"
 mkdir $BACKUP_DIR
-cp -r * $BACKUP_DIR
+cp -r * 
 
 # Étape 2: Récupérer le dépôt distant et copier seulement les fichiers qui sont différents
 
-REMOTE_REPO="https://github.com/eesnault1/blog-jardinage-nuxt"
+REMOTE_REPO="https://github.com/eesnault1/blog-template-nuxt"
+EXCLUDE=("public/*" ".env" "articles/*" "styles/main.css" "updateTemplate.sh")
 
 TMP_DIR="TMP_CLONE"
 mkdir $TMP_DIR
 git clone $REMOTE_REPO $TMP_DIR
 
-# Liste des fichiers/fichiers à exclure de la mise à jour
-EXCLUDE=("public/*" ".env" "articles/*")
+# Liste des fichiers à exclure de la mise à jour
+EXCLUDE_RSYNC=()
+for exclude_file in "${EXCLUDE[@]}"; do
+    EXCLUDE_RSYNC+=("--exclude=$exclude_file")
+done
 
-rsync -av --progress --exclude=${EXCLUDE[@]} $TMP_DIR/ ./ 
+rsync -av --progress "${EXCLUDE_RSYNC[@]}" $TMP_DIR/ ./ 
 
-# Etape 3 : supression du dossier temporaire 
-rm -rf $TMP_DIR
 
-# Etape 4 : Affichage du Readme.md de la mag 
+
+# Etape 3 : Affichage du Readme.md de la maj
 echo -e "\n\n=== README MAJ ===\n"
-cat $TMP_DIR/READMEMAJ.md
+cat ./READMEMAJ.md
+
+# Etape 4 : supression du dossier temporaire 
+rm -rf $TMP_DIR
