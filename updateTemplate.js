5476783d4159d84435d5156f8c190319d8b17a4d
eesnault1 <erwanesnault1@gmail.com>
2023-08-26 20:41:09 +0900

    V1.0.1

--- updateTemplate.js
+++ updateTemplate.js
@@ -4,7 +4,7 @@ const simpleGit = require('simple-git');
 
 async function main() {
     const REMOTE_REPO = "https://github.com/eesnault1/blog-template-nuxt";
-    const EXCLUDE = new Set(["public/", ".env", "articles/", "styles/main.css", "updateTemplate.js", ".git"]);
+    const EXCLUDE = new Set(["public", ".env", "content", "styles/main.css", "updateTemplate.js", ".git", "data", ".gitignore"]);
     const TMP_DIR = "TMP_CLONE";
 
     const git = simpleGit();
@@ -17,6 +17,7 @@ async function main() {
         process.exit(1);
     }
 
+
 // Fonction pour copier récursivement les fichiers et les dossiers d'un chemin donné.
 const copyFilesRecursively = async (startPath) => {
     // Lire le contenu du dossier à partir du chemin donné.
@@ -26,25 +27,31 @@ const copyFilesRecursively = async (startPath) => {
     for (const fileOrDir of filesAndDirs) {
         // Créer le chemin complet vers l'élément actuel.
         const filePath = path.join(startPath, fileOrDir);
-        
+
+        const relativePath = filePath.replace(TMP_DIR + path.sep, "");
+        if (EXCLUDE.has(fileOrDir) || EXCLUDE.has(relativePath)) {
+            console.log(filePath)
+            continue;
+        }
         // Obtenir les statistiques de l'élément pour déterminer s'il s'agit d'un fichier ou d'un dossier.
         const stats = await fs.stat(filePath);
-        
-        // Si l'élément est un fichier...
-        if (stats.isFile()) {
-            // ... vérifier s'il n'est pas dans la liste des éléments à exclure.
-            if (!EXCLUDE.has(filePath.replace(TMP_DIR + "/", ""))) {
-                // Si le fichier n'est pas exclu, lire son contenu.
-                const fileContent = await fs.readFile(filePath, 'utf8');
-                // Écrire le contenu du fichier dans le dossier cible tout en remplaçant le nom du dossier temporaire par le dossier racine.
-                await fs.outputFile(path.join('.', filePath.replace(TMP_DIR, "")), fileContent);
+
+        // Verifier quels fichiers peuvent être lu
+                    // Si l'élément est un fichier...
+            if (stats.isFile()) {
+                    // Si le fichier n'est pas exclu, lire son contenu.
+                    const fileContent = await fs.readFile(filePath, 'utf8');
+                    // Écrire le contenu du fichier dans le dossier cible tout en remplaçant le nom du dossier temporaire par le dossier racine.
+                    await fs.outputFile(path.join('.', filePath.replace(TMP_DIR, "")), fileContent);
             }
-        }
-        // Si l'élément est un dossier...
-        else if (stats.isDirectory()) {
-            // ... appeler récursivement la fonction pour copier le contenu de ce dossier.
-            await copyFilesRecursively(filePath);
-        }
+            // Si l'élément est un dossier...
+            else if (stats.isDirectory()) {
+                // ... appeler récursivement la fonction pour copier le contenu de ce dossier.
+                await copyFilesRecursively(filePath);
+            }
+
+        
+
     }
 };
 
